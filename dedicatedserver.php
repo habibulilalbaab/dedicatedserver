bc700cdc2b7577b1a8995891179f15c51458f7e7
Muhammad Habib Ulil A <habibulilalbaab@intprism.com>
2023-09-10 22:26:46 +0700

    dd

--- dedicatedserver.php
+++ dedicatedserver.php
@@ -5,14 +5,16 @@ if (!defined("WHMCS")) {
     die("This file cannot be accessed directly");
 }
 
-
-// Mendapatkan admin notes untuk produk dengan ID tertentu
-$this->$adminNotes = Capsule::table('tblhosting')
-	->where('id', $params['serviceid'])
-	->value('notes');
-if ($this->$adminNotes) {
-	// Memisahkan admin notes menjadi baris-baris
-	$this->$notesLines = explode("\n", $adminNotes);
+function adminNotes(){
+	// Mendapatkan admin notes untuk produk dengan ID tertentu
+	$adminNotes = Capsule::table('tblhosting')
+		->where('id', $params['serviceid'])
+		->value('notes');
+	if ($adminNotes) {
+		// Memisahkan admin notes menjadi baris-baris
+		$notesLines = explode("\n", $adminNotes);
+	}
+	return $notesLines;
 }
 
 function dedicatedserver_MetaData(){
@@ -32,7 +34,7 @@ function dedicatedserver_ClientArea($params) {
 
 function dedicatedserver_startNoVNC($params) {
 	try {
-		shell_exec('cd ../modules/servers/dedicatedserver && nohup ./novnc/utils/novnc_proxy  --listen '.$params['serviceid'].' --vnc '.$notesLines[0].':'.$notesLines[1].' --ssl-only --heartbeat 3 --web-auth --auth-plugin BasicHTTPAuth --auth-source '.$notesLines[3].':'.$notesLines[4].'  > /dev/null 2>&1 &');
+		shell_exec('cd ../modules/servers/dedicatedserver && nohup ./novnc/utils/novnc_proxy  --listen '.$params['serviceid'].' --vnc '.adminNotes()[0].':'.adminNotes()[1].' --ssl-only --heartbeat 3 --web-auth --auth-plugin BasicHTTPAuth --auth-source '.adminNotes()[3].':'.adminNotes()[4].'  > /dev/null 2>&1 &');
 		return 'success';
 	} catch (\Throwable $th) {
 		//throw $th;
@@ -60,7 +62,7 @@ function dedicatedserver_AdminCustomButtonArray() {
 	return $buttonarray;
 }
 function dedicatedserver_AdminServicesTabFields($params) {
-	$userpass = str_replace(array("\n", "\r"), '', $notesLines[3].":".$notesLines[4]."@");
+	$userpass = str_replace(array("\n", "\r"), '', adminNotes()[3].":".adminNotes()[4]."@");
     $fieldsarray = array(
         'API Connection Status' => '<div class="successbox">VNC Connection OK</div>',
         'Connection information' =>
@@ -69,17 +71,17 @@ function dedicatedserver_AdminServicesTabFields($params) {
 
 			<tr>
 				<td><b>VNC Server:</b></td>
-				<td>' . $notesLines[0] . '</td>
+				<td>' . adminNotes()[0] . '</td>
 			</tr>
 
 			<tr>
 				<td><b>VNC Port:</b></td>
-				<td>' . $notesLines[1] . '</td>
+				<td>' . adminNotes()[1] . '</td>
 			</tr>
 
 			<tr>
 				<td><b>VNC Password:</b></td>
-				<td>' . $notesLines[2] . '</td>
+				<td>' . adminNotes()[2] . '</td>
 			</tr>
 
 			<tr>
@@ -90,12 +92,12 @@ function dedicatedserver_AdminServicesTabFields($params) {
 
 			<tr>
 				<td><b>NoVNC User:</b></td>
-				<td>' . $notesLines[3] . '</td>
+				<td>' . adminNotes()[3] . '</td>
 			</tr>
 
 			<tr>
 				<td><b>NoVNC Password:</b></td>
-				<td>' . $notesLines[4] . '</td>
+				<td>' . adminNotes()[4] . '</td>
 			</tr>
 
 			<tr>
