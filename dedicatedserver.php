f91711371a268cc0091a00adf495ce6203686b0f
Muhammad Habib Ulil A <habibulilalbaab@intprism.com>
2023-09-10 22:30:00 +0700

    ddd

--- dedicatedserver.php
+++ dedicatedserver.php
@@ -34,7 +34,7 @@ function dedicatedserver_ClientArea($params) {
 
 function dedicatedserver_startNoVNC($params) {
 	try {
-		shell_exec('cd ../modules/servers/dedicatedserver && nohup ./novnc/utils/novnc_proxy  --listen '.$params['serviceid'].' --vnc '.adminNotes()[0].':'.adminNotes()[1].' --ssl-only --heartbeat 3 --web-auth --auth-plugin BasicHTTPAuth --auth-source '.adminNotes()[3].':'.adminNotes()[4].'  > /dev/null 2>&1 &');
+		shell_exec('cd ../modules/servers/dedicatedserver && nohup ./novnc/utils/novnc_proxy  --listen '.$params['serviceid'].' --vnc '.adminNotes($params)[0].':'.adminNotes($params)[1].' --ssl-only --heartbeat 3 --web-auth --auth-plugin BasicHTTPAuth --auth-source '.adminNotes($params)[3].':'.adminNotes($params)[4].'  > /dev/null 2>&1 &');
 		return 'success';
 	} catch (\Throwable $th) {
 		//throw $th;
@@ -62,7 +62,7 @@ function dedicatedserver_AdminCustomButtonArray() {
 	return $buttonarray;
 }
 function dedicatedserver_AdminServicesTabFields($params) {
-	$userpass = str_replace(array("\n", "\r"), '', adminNotes()[3].":".adminNotes()[4]."@");
+	$userpass = str_replace(array("\n", "\r"), '', adminNotes($params)[3].":".adminNotes($params)[4]."@");
     $fieldsarray = array(
         'API Connection Status' => '<div class="successbox">VNC Connection OK</div>',
         'Connection information' =>
@@ -71,17 +71,17 @@ function dedicatedserver_AdminServicesTabFields($params) {
 
 			<tr>
 				<td><b>VNC Server:</b></td>
-				<td>' . adminNotes()[0] . '</td>
+				<td>' . adminNotes($params)[0] . '</td>
 			</tr>
 
 			<tr>
 				<td><b>VNC Port:</b></td>
-				<td>' . adminNotes()[1] . '</td>
+				<td>' . adminNotes($params)[1] . '</td>
 			</tr>
 
 			<tr>
 				<td><b>VNC Password:</b></td>
-				<td>' . adminNotes()[2] . '</td>
+				<td>' . adminNotes($params)[2] . '</td>
 			</tr>
 
 			<tr>
@@ -92,12 +92,12 @@ function dedicatedserver_AdminServicesTabFields($params) {
 
 			<tr>
 				<td><b>NoVNC User:</b></td>
-				<td>' . adminNotes()[3] . '</td>
+				<td>' . adminNotes($params)[3] . '</td>
 			</tr>
 
 			<tr>
 				<td><b>NoVNC Password:</b></td>
-				<td>' . adminNotes()[4] . '</td>
+				<td>' . adminNotes($params)[4] . '</td>
 			</tr>
 
 			<tr>
