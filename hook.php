719201146f388bc67c765e1156230680b466b95e
Muhammad Habib Ulil A <habibulilalbaab@intprism.com>
2023-09-11 02:19:50 +0700

    jj

--- hook.php
+++ hook.php
@@ -6,7 +6,7 @@ add_hook('ClientAreaPage', 1, 'customButtonHook');
 function customButtonHook($vars) {
     // Cek apakah tombol "Create" diklik
     if (isset($_POST['runNoVNC'])) {
-        dedicatedserver_startNoVNC($params)
+        dedicatedserver_startNoVNC($params);
     }
 }
 
