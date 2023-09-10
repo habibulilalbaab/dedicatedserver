65c71aad79b65a651280e62f7696aa6e1b56dfe6
Muhammad Habib Ulil A <habibulilalbaab@intprism.com>
2023-09-11 00:56:32 +0700

    vvv

--- hook.php
+++ hook.php
@@ -1,29 +1,30 @@
 <?php
+function activate() {
+    // Daftarkan hook 'ClientAreaPage'
+    add_hook('ClientAreaPage', 1, $this->module_name . '_client_area_hook');
+    return array('status' => 'success', 'description' => 'Modul berhasil diaktifkan');
+}
+
+function module_name_client_area_hook($vars) {
+    if ($vars['filename'] === 'clientareaproductdetails' && isset($_GET['id']) && $_GET['id'] === '218') {
+        // Tambahkan tombol "Start NoVNC" dengan tautan ke fungsi dedicatedserver_startNoVNC()
+        $productId = (int) $_GET['id'];
+        echo '<a href="clientarea.php?action=start_novnc&id=' . $productId . '" class="btn btn-primary">Start NoVNC</a>';
+    }
+}
+
 add_hook('ClientAreaPage', 1, function($vars) {
-    // Periksa apakah halaman detail layanan sedang ditampilkan
-    if ($vars['action'] === 'productdetails') {
-        // Ambil ID produk dari variabel WHMCS
+    if ($_GET['action'] === 'start_novnc' && isset($_GET['id'])) {
         $productId = (int) $_GET['id'];
 
-        // Tambahkan tombol dan JavaScript
-        echo '<a href="clientarea.php?action=custom_action&product_id=' . $productId . '" class="btn btn-primary" id="customButton">Tombol Kustom</a>';
-        echo '<script>
-                  document.getElementById("customButton").addEventListener("click", function(event) {
-                      event.preventDefault(); // Untuk mencegah perubahan URL
-                      // Kirim permintaan ke aksi kustom di sini
-                      fetch("clientarea.php?action=custom_action&product_id=' . $productId . '")
-                          .then(function(response) {
-                              if (response.ok) {
-                                  alert("Tindakan berhasil dijalankan.");
-                              } else {
-                                  alert("Tindakan gagal.");
-                              }
-                          })
-                          .catch(function(error) {
-                              console.error("Terjadi kesalahan:", error);
-                          });
-                  });
-              </script>';
+        // Panggil fungsi dedicatedserver_startNoVNC() dengan ID produk yang sesuai
+        $result = dedicatedserver_startNoVNC($productId);
+
+        if ($result === true) {
+            echo '<script>alert("NoVNC telah berhasil dimulai.");</script>';
+        } else {
+            echo '<script>alert("Gagal memulai NoVNC.");</script>';
+        }
     }
 });
 
