41af5c0d1fd05b9cff86a5983cfa8e472499d107
Muhammad Habib Ulil A <habibulilalbaab@intprism.com>
2023-09-11 02:26:54 +0700

    j

--- hook.php
+++ hook.php
@@ -3,6 +3,7 @@ include "dedicatedserver.php";
 
 function customButtonHook($vars) {
     // Cek apakah tombol "Create" diklik
+    error_log('Hook is running');
     if (isset($_POST['runNoVNC'])) {
         dedicatedserver_startNoVNC($params);
         echo 'success';
