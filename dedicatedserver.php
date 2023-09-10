be06f446f4643e7d13c0e37111e4f116ab67814b
Muhammad Habib Ulil A <habibulilalbaab@intprism.com>
2023-09-10 21:36:47 +0700

    dd

--- dedicatedserver.php
+++ dedicatedserver.php
@@ -25,7 +25,7 @@ function dedicatedserver_startNoVNC($params) {
 }
 function dedicatedserver_stopNoVNC($params) {	
 	$proccess = shell_exec("ps aux | grep -i 'novnc_proxy  --listen 1111'");
-	$pid = explode(" ", $proccess)[1];
+	$pid = explode(" ", $proccess)[0];
 	// kill
 	$kill = shell_exec("kill -9".$pid." && kill $(lsof -t -i:1111)");
 	return $pid;
