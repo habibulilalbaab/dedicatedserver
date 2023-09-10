1d6bd2d37485cfa3909cebf1ce45b8b19f2e24da
Muhammad Habib Ulil A <habibulilalbaab@intprism.com>
2023-09-10 21:07:52 +0700

    nn

--- dedicatedserver.php
+++ dedicatedserver.php
@@ -20,8 +20,8 @@ function dedicatedserver_ClientArea($params) {
   );
 }
 function dedicatedserver_startNoVNC($params) {
-	shell_exec('cd ../modules/servers/dedicatedserver && ./novnc/utils/novnc_proxy  --listen 1111 --vnc 10.255.255.54:5909 --ssl-only --heartbeat 3 --web-auth --auth-plugin BasicHTTPAuth --auth-source username:password  > /dev/null 2>&1 &');
-	return 'success';
+	$tes = shell_exec('cd ../modules/servers/dedicatedserver && ./novnc/utils/novnc_proxy  --listen 1111 --vnc 10.255.255.54:5909 --ssl-only --heartbeat 3 --web-auth --auth-plugin BasicHTTPAuth --auth-source username:password  > /dev/null 2>&1 &');
+	return $tes;
 }
 function dedicatedserver_stopNoVNC($params) {	
 	$proccess = shell_exec("ps aux | grep -i 'novnc_proxy  --listen 1111'");
