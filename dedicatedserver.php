1865bd5f749eeb7b2a397e75b483e9d05d8b413e
Muhammad Habib Ulil A <habibulilalbaab@intprism.com>
2023-09-13 12:23:58 +0700

    aaa

--- dedicatedserver.php
+++ dedicatedserver.php
@@ -49,7 +49,7 @@ function dedicatedserver_startNoVNC($params) {
 		$userpass = str_replace(array("\n", "\r"), '', adminNotes($params)[3].":".adminNotes($params)[4]);
 		$vncserver = str_replace(array("\n", "\r"), '', adminNotes($params)[0].":".adminNotes($params)[1]);
 		$port = $params['serviceid']+1000;
-		$command = "cd ".$_SERVER['DOCUMENT_ROOT']."/modules/servers/dedicatedserver && nohup ./novnc/utils/novnc_proxy  --listen ".$port." --vnc ".$vncserver." --ssl-only --heartbeat 3 --web-auth --auth-plugin BasicHTTPAuth --auth-source ".$userpass."  > /dev/null 2>&1 &";
+		$command = "cd ../../modules/servers/dedicatedserver && nohup ./novnc/utils/novnc_proxy  --listen ".$port." --vnc ".$vncserver." --ssl-only --heartbeat 3 --web-auth --auth-plugin BasicHTTPAuth --auth-source ".$userpass."  > /dev/null 2>&1 &";
 		shell_exec($command);
 		return $command;
 		//return 'success';
